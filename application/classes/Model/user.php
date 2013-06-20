a724cdd1c2e76949960e361fd80f322483441ead
Kevin Chen <kevinchen8223@gmail.com>
2013-06-19 17:03:25 -0700

    took out useless crap

--- application/classes/Model/user.php
+++ application/classes/Model/user.php
@@ -11,19 +11,4 @@ class Model_User extends Model_Auth_User {
 	    'roles'       => array('model' => 'role', 'through' => 'roles_users'),
 	);
 
-	 /**
-     * Allows a model use both email and username as unique identifiers for login
-     *
-     * @param   string  unique value
-     * @return  string  field name
-     */
-    public function unique_key($value)
-    {
-        return Valid::email($value) ? 'email' : 'username';
-    }
-
-
-}
-
-
 ?>
